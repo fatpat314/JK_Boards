a7841b8212606616f6995fd144d98171d0938d78
Patrick <w.patrick.kelly@gmail.com>
2021-01-09 22:43:33 -0800

    redo repo

--- JK_boards/settings.py
+++ JK_boards/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'payment',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -124,12 +125,39 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
 """                                                       V these is the real keys V"""
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_TEST_PUBLIC_KEY') #os.getenv('STRIPE_PUBLIC_KEY')
-STRIPE_PRIVATE_KEY = os.getenv('STRIPE_TEST_PRIVATE_KEY') #os.getenv('STRIPE_PRIVATE_KEY')
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY') #os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_PRIVATE_KEY = os.getenv('STRIPE_PRIVATE_KEY') #os.getenv('STRIPE_PRIVATE_KEY')
 MAILGUN_PRIVATE_KEY = os.getenv('MAILGUN_PRIVATE_KEY')
+
+AWS_PUBLIC_KEY = os.getenv('AWS_PUBLIC_KEY')
+AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backend.s3boto3.S3Boto3Storage'
