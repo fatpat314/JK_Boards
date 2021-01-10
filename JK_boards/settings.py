59671de80459f81f8e8d9106baaa791c500b0b4c
Patrick <w.patrick.kelly@gmail.com>
2021-01-09 21:58:20 -0800

    heroku

--- JK_boards/settings.py
+++ JK_boards/settings.py
@@ -160,4 +160,4 @@ BUCKET_NAME = os.getenv('jkboards')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backend.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
